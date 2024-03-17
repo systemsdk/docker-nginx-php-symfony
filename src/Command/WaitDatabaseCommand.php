880a2e0f172c0615930d60ce5f94a5d188293def
DKravtsov <dmitriy.kravtsov@systemsdk.com>
2024-03-17 19:40:22 +0200

    small refactoring.

--- src/Command/WaitDatabaseCommand.php
+++ src/Command/WaitDatabaseCommand.php
@@ -24,7 +24,7 @@ class WaitDatabaseCommand extends Command
     /**
      * Wait sleep time for db connection in seconds
      */
-    private const WAIT_SLEEP_TIME = 2;
+    private const int WAIT_SLEEP_TIME = 2;
 
     /**
      * Constructor
